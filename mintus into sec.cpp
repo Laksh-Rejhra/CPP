a7dfce6795abae0f9f9a04ae5bfdd4c7a659f672
Laksh-Rejhra <144317943+Laksh-Rejhra@users.noreply.github.com>
2023-11-18 07:44:28 -0800

    Add files via upload

---
+++ mintus into sec.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	//	convert Minutes into Seconds
+	int sec=60;
+	int minu;
+	cout<<"Enter Minutes and convert it into seconds "<<endl;
+	cin>>minu;
+	
+	sec*=minu;
+	
+	cout<<"minutes = "<<minu<<endl;
+	cout<<"Total seconds = "<<sec<<endl;
+
+
+}
+
